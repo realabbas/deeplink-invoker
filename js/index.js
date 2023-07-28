c5ab771a02ed6ad1308808fd478372b9e82f41eb
Ali Abbas <irizviali@gmail.com>
2023-07-28 09:47:38 +0000

    feat: add google play store and app store input
    Signed-off-by: Ali Abbas <irizviali@gmail.com>

--- js/index.js
+++ js/index.js
@@ -1,22 +1,33 @@
 function invokeAppScheme() {
-    var appScheme = document.getElementById("appSchemeInput").value;
-    var payload = document.getElementById("payloadInput").value;
-    if (appScheme) {
+  var appScheme = document.getElementById("appSchemeInput").value;
+  var payload = document.getElementById("payloadInput").value;
+  var androidAppStoreLink = document.getElementById("androidAppStoreLinkInput").value;
+  var iosAppStoreLink = document.getElementById("iosAppStoreLinkInput").value;
+
+  if (appScheme) {
       if (payload) {
-        appScheme += "?" + payload;
+          appScheme += "?" + payload;
       }
+
       // Attempt to open the app
       window.location.href = appScheme;
+
       // Timeout to redirect to app store if the app is not installed
-      setTimeout(function () {
-        window.location.href = "https://your-app-store-link"; // Replace with your app's store link
+      setTimeout(function() {
+          var isAndroid = /Android/i.test(navigator.userAgent);
+          if (isAndroid) {
+              window.location.href = androidAppStoreLink;
+          } else {
+              window.location.href = iosAppStoreLink;
+          }
       }, 2000); // Adjust the timeout duration (milliseconds) as needed
-    } else {
+  } else {
       alert("Please enter an app scheme!");
-    }
   }
-  // Show instructions on page load
-  document.addEventListener("DOMContentLoaded", function () {
-    var instructions = document.querySelector(".instructions");
-    instructions.style.display = "block";
-  });
+}
+
+// Show instructions on page load
+document.addEventListener("DOMContentLoaded", function () {
+  var instructions = document.querySelector(".instructions");
+  instructions.style.display = "block";
+});
